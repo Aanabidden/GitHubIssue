44128bc6b6896b53ca83546a0fdddb04a5c502e8
Aradhana <aradhanaverma@eternalrobotics.com>
2021-01-20 22:17:19 +0530

    Add pod file and realm model

---
+++ Pods/Target Support Files/Pods-GitHubIssue-GitHubIssueUITests/Pods-GitHubIssue-GitHubIssueUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GitHubIssue_GitHubIssueUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GitHubIssue_GitHubIssueUITests
+@end
---
+++ Pods/Target Support Files/Pods-GitHubIssue/Pods-GitHubIssue-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GitHubIssue : NSObject
+@end
+@implementation PodsDummy_Pods_GitHubIssue
+@end
